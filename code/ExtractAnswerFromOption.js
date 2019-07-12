ed676e39c9260f50f1da4a7601805f2734664f49
jonmpan <jonmpan@gmail.com>
2019-07-12 10:11:31 -0700

    original quiz code

--- code/ExtractAnswerFromOption.js
+++
@@ -1,4 +0,0 @@
-//Convert the option structure to a text answer.
-module.exports.function = function extractAnswerFromOption (option) {
-  return option.text;
-}
--- code/StartQuiz.js
+++ code/StartQuiz.js
@@ -1,3 +1,18 @@
-module.exports.function = function startQuiz (quiz) {
-  return quiz;
+//Start the quiz. Initialize the quiz state.
+exports.function = function(quiz) {
+  state = {
+    quiz: quiz,
+    started: true,
+    completed: quiz.questions.length == 0,
+    currentQuestion: quiz.questions[0],
+    lastScore: null,
+    scoreBook: {
+      scores: [],
+      correctAnswerCount: 0,
+      questionsAnsweredCount: 0,
+      questionsLeftCount: quiz.questions.length,
+    }
+  }
+
+  return state
 }
